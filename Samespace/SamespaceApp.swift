68b2c7b8c74ba184b1e7a976c012890a63dc2796
Nishant Minerva <nishantcafe@gmail.com>
2023-10-22 18:47:06 +0530

    Initial Commit

---
+++ Samespace/SamespaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamespaceApp.swift
+//  Samespace
+//
+//  Created by Nishant Minerva on 22/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SamespaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
